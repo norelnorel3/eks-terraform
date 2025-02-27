bc8ba3a2b58ff7dccc7db81ea9f0b0a8384d046f
norelm <norelm@rafael.co.il>
2025-02-27 22:16:44 +0200

    working-prod-environment

--- terragrunt-eks/env/prod/karpenter/.terragrunt-cache/hs3U0XYqX7fOcZusVPO9rsRGur0/ExhbSWZiHyGic0ddOH7FhrXoPwk/providers.tf
+++ terragrunt-eks/env/prod/karpenter/.terragrunt-cache/hs3U0XYqX7fOcZusVPO9rsRGur0/ExhbSWZiHyGic0ddOH7FhrXoPwk/providers.tf
@@ -29,3 +29,16 @@ provider "helm" {
     }
   }
 }
+
+# provider "kubectl" {
+#   apply_retry_count      = 15
+#   host                   = var.cluster_endpoint
+#   cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+#   load_config_file       = false
+
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "aws"
+#     args = ["eks", "get-token", "--cluster-name", var.cluster_name]
+#   }
+# } 
